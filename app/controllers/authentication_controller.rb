22095a39eab853bd0accb8b67c531a740f4c1e3f
derikbf <derikb@gmail.com>
2022-01-17 23:11:34 -0300

    Updated authenticationController User | Json

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,16 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_request
+
+  def authenticate
+
+     command = AuthenticateUser.call(params[:email], params[:password])
+
+     p command 
+
+     if command.success?
+       render json: { auth_token: command.result }
+     else
+        render json: { error: command.errors }, status: :unauthorized
+     end
+  end
+end
